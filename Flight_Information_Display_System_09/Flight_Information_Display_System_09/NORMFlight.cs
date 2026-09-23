681a8bdd03221f6a7718bb508178db2071523344
wyn-cmd <wwjdholiday@gmail.com>
2026-09-23 09:47:37 +0800

    tidy up flight fee calculation logic

--- Flight_Information_Display_System_09/Flight_Information_Display_System_09/NORMFlight.cs
+++ Flight_Information_Display_System_09/Flight_Information_Display_System_09/NORMFlight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,25 +12,30 @@ using System.Threading.Tasks;
 
 namespace S10265740_PRG2Assignment
 {
+    // Represents a normal flight with standard fee structures.
     public class NORMFlight : Flight
     {
         public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time", string specialRequestCode = "None", string boardingGate = "Unassigned")
             : base(flightNumber, origin, destination, expectedTime, status, specialRequestCode, boardingGate)
         {
-            FlightNumber = flightNumber;
-            Origin = origin;
-            Destination = destination;
-            ExpectedTime = expectedTime;
-            Status = status;
         }
 
         public override double CalculateFees()
         {
             double boardingGateBaseFee = 300.0;
-            double additionalFee = (Destination == "Singapore (SIN)") ? 500.0 : (Origin == "Singapore (SIN)") ? 800.0 : 0.0;
+            double additionalFee = 0.0;
+
+            if (Destination == "Singapore (SIN)")
+            {
+                additionalFee = 500.0;
+            }
+            else if (Origin == "Singapore (SIN)")
+            {
+                additionalFee = 800.0;
+            }
+
             return boardingGateBaseFee + additionalFee;
         }
-
 
         public override string ToString()
         {
